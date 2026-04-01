65c079bb938c01fe4e13ccc5b548eae5c4544449
kozal2026 <erdalkozal5@hotmail.com>
2026-04-01 16:52:01 +0300

    Update yarisma.py

--- yarisma.py
+++ yarisma.py
@@ -174,7 +174,7 @@ if 'secili_sorular' not in st.session_state:
     st.session_state.gizli_siklar = []
 
 # --- ARAYÜZ ---
-st.markdown('<h2 style="text-align:center; color:#11114e;">💰 Milyoner</h2>', unsafe_allow_html=True)
+st.markdown('<h2 style="text-align:center; color:#11114e;">💰 Milyoner Benden Para İsteme Sonra</h2>', unsafe_allow_html=True)
 
 if not st.session_state.elendi and st.session_state.index < 12:
     soru = st.session_state.secili_sorular[st.session_state.index]
